51c78f82d3fac7693fbcdf1ed9391356d5b5ab43
ybchen1 <ybchen1@chinaums.com>
2023-05-31 12:39:04 +0800

    first commit

---
+++ src/main/java/org/ybchen1/Main.java
@@ -0,0 +1,11 @@
+package org.ybchen1;
+
+/**
+ * @Author: ybchen
+ * @Date: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
