59a063e1dda679888ce9d68a6622e09456e95ef5
ybchen1 <ybchen1@chinaums.com>
2023-06-01 16:23:37 +0800

    设计模式之单例模式

---
+++ src/main/java/org/ybchen1/singleton/Client.java
@@ -0,0 +1,16 @@
+package org.ybchen1.singleton;
+
+/**
+ * @Author: ybchen
+ * @Date: 2023/6/1 16:19
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Singleton singletonInstance=Singleton.getInstance();
+        Singleton singletonInstance1=Singleton.getInstance();
+        System.out.println(singletonInstance1==singletonInstance);
+
+
+    }
+}
---
+++ src/main/java/org/ybchen1/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package org.ybchen1.singleton;
+
+/**
+ * @Author: ybchen
+ * @Date: 2023/6/1 16:17
+ */
+
+/**
+ * hungry method
+ */
+public class Singleton {
+    private static Singleton instance = new Singleton();
+    private Singleton(){}
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+}
