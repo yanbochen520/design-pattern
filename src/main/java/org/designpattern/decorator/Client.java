5bb9fcbe396bf018ed0b9f82dd3830dfae951f2b
ybchen1 <ybchen1@chinaums.com>
2023-06-02 15:26:41 +0800

    设计模式之装饰器模式2

--- src/main/java/org/designpattern/decorator/Client.java
+++ src/main/java/org/designpattern/decorator/Client.java
@@ -9,6 +9,8 @@ public class Client {
       Component undecorator=new UnDecorator("undecorator");
       Component decorator1= new Decorator("decorator1",undecorator);
       Component decorator2= new Decorator("decorator2",decorator1);
+      System.out.println(undecorator.show());
+      System.out.println(decorator1.show());
       System.out.println(decorator2.show());
 
 
